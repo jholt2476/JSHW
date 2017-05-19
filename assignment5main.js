abdaae739b1b05547b3634e28976c8d4a34a49d4
jholt2476 <Jackson@Jacksons-MacBook-Pro.local>
2017-05-18 21:07:27 -0400

    Final Commit
    Homework

--- assignment5main.js
+++ assignment5main.js
@@ -8,36 +8,45 @@ var twoPlusTwo = addNumbers(2, 2)
 console.log(twoPlusTwo)
  // 4
 
-var yell = {};
-yell.Uppercase="Yell"
-yell.timesten="Yell10" + "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"
-+ "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"
 
-console.log(yell.Uppercase)
+function yell(word){
+  return (word.toUpperCase())
+}
 
-console.log(yell.timesten);
+function yell10(words){
+  for (var i = 0; i< 10; i++){
+    var word = yell(words)
+    console.log(word);
+  }
+}
 
+console.log(yell("word"));
+yell10("something");
 
 
 function longest(input1, input2) {
-if(input1.length>input2.length)
-  return input1
+  if(input1.length>input2.length){
+    return input1
+  }
+  else if(input2.length>input1.length) {
+    return input2
+  }
 }
-elseif(input2.length>input1.length) {
-  return input2
-}
-var appleProducts + {"iMac", "iTouch", "MacBook Pro"};
+
+var appleProducts = ["iMac", "iTouch", "MacBook Pro"];
 var instructor = "Joshua";
 
-var result = longest (appleProducts, instructor);
+var result = longest(appleProducts, instructor);
 console.log(result);
-
-<script src="assignmentmain.js"></script>
 
 
 function isVowel(char){
-  if (char === "A" || char === "E" || char === "I"char === "O" || char === "U" ||
-  char === "a"|| char === "e" || char === "i" || char === "o" || char === "u")
-  return true;
+  if (char === "A" || char === "E" || char === "I" || char === "O" || char === "U" ||
+  char === "a"|| char === "e" || char === "i" || char === "o" || char === "u"){
+    return true;
+  }
+  else {
+    return false
+  }
 }
 var result = isVowel("A")
