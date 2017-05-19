6a4e44ad33ecfe31ce42f93bab2f81f5e4f21937
jholt2476 <Jackson@Jacksons-MacBook-Pro.local>
2017-05-18 20:21:49 -0400

    Commit
    HomeWork Assignment 5

---
+++ assignment5main.js
@@ -0,0 +1,41 @@
+
+function addNumbers(numberA, numberB) {
+  console.log(numberA + numberB);
+}
+
+var twoPlusTwo = {addNumbers(2, 2)
+};
+
+console.log(twoPlusTwo)
+ // 4
+
+
+
+// function yell (){
+//   return "Yell"
+// }
+//
+// function isVowel(char){
+//   if (char === "A" || char === "E" || char === "I"char === "O" || char === "U" ||
+//   char === "a"|| char === "e" || char === "i" || char === "o" || char === "u")
+//   return true;
+//
+// var result = isVowel("A")
+//
+//
+// function longest(input1, input2) {
+// if(input1.length>input2.length) {
+//   return input1
+// }
+// elseif(input2.length>input1.length) {
+//   return input2
+// }
+//
+// }
+// var appleProducts + {"iMac", "iTouch", "MacBook Pro"};
+// var instructor = "Joshua";
+//
+// var result = longest (appleProducts, instructor);
+// console.log(result);
+//
+// <script src="main2.js"></script>
