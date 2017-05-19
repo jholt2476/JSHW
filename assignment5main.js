4fdeaa22cf588cf980e4ada7632ae47c329bdae7
jholt2476 <Jackson@Jacksons-MacBook-Pro.local>
2017-05-18 20:38:33 -0400

    Commit 2

--- assignment5main.js
+++ assignment5main.js
@@ -1,41 +1,43 @@
 
 function addNumbers(numberA, numberB) {
+  return numberA + numberB;
   console.log(numberA + numberB);
 }
-
-var twoPlusTwo = {addNumbers(2, 2)
-};
+var twoPlusTwo = addNumbers(2, 2)
 
 console.log(twoPlusTwo)
  // 4
 
+var yell = {};
+yell.Uppercase="Yell"
+yell.timesten="Yell10" + "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"
++ "Yell10"+ "Yell10"+ "Yell10"+ "Yell10"
+
+console.log(yell.Uppercase)
+
+console.log(yell.timesten);
 
 
-// function yell (){
-//   return "Yell"
-// }
-//
-// function isVowel(char){
-//   if (char === "A" || char === "E" || char === "I"char === "O" || char === "U" ||
-//   char === "a"|| char === "e" || char === "i" || char === "o" || char === "u")
-//   return true;
-//
-// var result = isVowel("A")
-//
-//
-// function longest(input1, input2) {
-// if(input1.length>input2.length) {
-//   return input1
-// }
-// elseif(input2.length>input1.length) {
-//   return input2
-// }
-//
-// }
-// var appleProducts + {"iMac", "iTouch", "MacBook Pro"};
-// var instructor = "Joshua";
-//
-// var result = longest (appleProducts, instructor);
-// console.log(result);
-//
-// <script src="main2.js"></script>
+
+function longest(input1, input2) {
+if(input1.length>input2.length)
+  return input1
+}
+elseif(input2.length>input1.length) {
+  return input2
+}
+var appleProducts + {"iMac", "iTouch", "MacBook Pro"};
+var instructor = "Joshua";
+
+var result = longest (appleProducts, instructor);
+console.log(result);
+
+<script src="assignmentmain.js"></script>
+
+
+function isVowel(char){
+  if (char === "A" || char === "E" || char === "I"char === "O" || char === "U" ||
+  char === "a"|| char === "e" || char === "i" || char === "o" || char === "u")
+  return true;
+}
+var result = isVowel("A")
